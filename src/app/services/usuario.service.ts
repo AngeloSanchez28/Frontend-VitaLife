4dcc430a4db76f43d6e3d5249c663298828a0689
Mabel-LL <mleonl5@upao.edu.pe>
2023-11-28 10:13:56 -0500

    service
    service

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -8,17 +8,19 @@ import {Usuario} from "../models/usuario";
 })
 export class UsuarioService {
 
-  private apiUrl= 'http://localhost:8080/api/habito';
-  private token = '';
+  private apiUrl= 'http://localhost:8080/usuario/verperfil/3';
+  private token = 'Mp9ox0pHtshkmgptH/c3xA9yEOqhLONVyeiStw8l5OKQD43tsjM4Rm0zJbFTa9VZIhZzPvdLXrXKsz5CxSWzxtl9TW8FuWiwN7YoI28SDPE3aXcN7Yoo1oUO33jpE2NsJcWFw2vl2UmMeLBj+d2fvTkYY8KcRwp0UFitzt4GFidaaMqd/omCZDxae0i99VLlku8D1/VvlkuEQdj5ysQuDTDKQ027M5LnzEiRMQfOi8VNWL7OolDCRlQjq0kAvPKaslID4H6WpPUwLpIQkIACQg==';
 
   constructor(private http: HttpClient) {}
 
   getUsuario(id: number): Observable<Usuario> {
-    const url = `${this.apiUrl}/usuarios/${id}`;
+    const url = `${this.apiUrl}`;
+    console.log(url);
+    console.log(this.token);
     const headers = new HttpHeaders({
       Authorization: `Bearer ${this.token}`,
     });
-    return this.http.get<Usuario>(url);
+    return this.http.get<Usuario>(url, { headers });
   }
 
 }
