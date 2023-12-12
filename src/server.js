0b3b3fe82702fc6c29dbe391ae3032bd308a7a46
AngeloSanchez28 <88355777+AngeloSanchez28@users.noreply.github.com>
2023-12-12 12:22:49 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/frontend-vita-life'));
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/frontend-vita-life/index.html'));
-});
+app.use(express.static('./dist/frontend-vita-life'));
+app.get('/*',(req,res) =>
+  res.sendFile('index.html', {root: 'dist/frontend-vita-life/'}),
+);
 
 app.listen(process.env.PORT || 8080);
