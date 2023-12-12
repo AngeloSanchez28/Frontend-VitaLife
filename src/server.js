bca99a7cc5a8759f50ba128643fd59ed39b3e00d
Angelo Sanchez <jsanchezs17@upao.edu.pe>
2023-12-12 12:08:44 -0500

    Despliegue Part2

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname+'/dist/frontend-vita-life'));
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/dist/frontend-vita-life/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
